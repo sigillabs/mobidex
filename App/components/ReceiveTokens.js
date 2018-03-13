a68554ea8173f97a8722d0fd73ae6f4817b35a3f
Abraham Elmahrek <abraham@elmahrek.com>
2018-03-13 09:13:26 -0700

    #29: Share address using Share API

--- App/components/ReceiveTokens.js
+++ App/components/ReceiveTokens.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { Card, Header, Icon, Text } from "react-native-elements";
+import { Share, View } from "react-native";
+import { Card, Header, Text } from "react-native-elements";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import { connect } from "react-redux";
 import ethUtil from "ethereumjs-util";
+import Button from "./Button";
 
 class ReceiveTokens extends Component {
+  share = () => {
+    Share.share({
+      message: `Here's My Address: ${this.props.address}`,
+      title: "My Address"
+    }, {
+      // Android only:
+      dialogTitle: "Mobidex Address",
+      // iOS only:
+      subject: "Mobidex Address",
+      excludedActivityTypes: [
+        "com.apple.UIKit.activity.PostToTwitter"
+      ]
+    })
+  };
+
   render() {
     let { address } = this.props;
     let uri = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${ethUtil.stripHexPrefix(this.props.address)}`;
@@ -24,6 +41,12 @@ class ReceiveTokens extends Component {
         <View style={[{ justifyContent: "center", alignItems: "center" }]}>
           <Text h4>Address</Text>
           <Text>{this.props.address}</Text>
+          <Button
+            large
+            icon={<Icon name="send" size={20} color="white" />}
+            onPress={this.share}
+            text="Share Address"
+            style={{ marginTop: 10 }} />
         </View>
       </Card>
     );
