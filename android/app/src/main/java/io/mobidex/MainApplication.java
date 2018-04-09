4669e444e8a70123c0b02d6b96f4cc77ae9ba87f
Abraham Elmahrek <abraham@elmahrek.com>
2018-04-08 22:12:26 -0700

    ios relink and config

--- android/app/src/main/java/io/mobidex/MainApplication.java
+++ android/app/src/main/java/io/mobidex/MainApplication.java
@@ -3,8 +3,8 @@ package io.mobidex;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
           new EncryptionManagerPackage(),
           new VectorIconsPackage(),
           new RandomBytesPackage()
