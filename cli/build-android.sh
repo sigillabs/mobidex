4830021fee8945b40d74bbefa884951509170854
Abraham Elmahrek <abraham@elmahrek.com>
2018-06-15 14:21:24 -0700

    Get android compiling again

---
+++ cli/build-android.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pushd android
+
+read -sp 'If you would like to sign the APK, please add your store passphrase now: ' MOBIDEX_RELEASE_STORE_PASSWORD
+echo ""
+if [ -z ${MOBIDEX_RELEASE_STORE_PASSWORD+x} ]; then
+  ./gradlew clean assembleRelease -PMOBIDEX_SIGN=0
+else
+  read -p 'Mobidex keystore path relative to ./android/app (default: ../../mobidex.keystore): ' MOBIDEX_RELEASE_STORE_FILE
+  read -p 'Mobidex keystore alias (default: mobidex): ' MOBIDEX_RELEASE_KEY_ALIAS
+  echo ""
+
+  echo "./gradlew assembleRelease -PMOBIDEX_RELEASE_KEY_ALIAS=${MOBIDEX_RELEASE_KEY_ALIAS+mobidex} -PMOBIDEX_RELEASE_STORE_FILE=${MOBIDEX_RELEASE_STORE_FILE+../mobidex.keystore} -PMOBIDEX_RELEASE_STORE_PASSWORD= -PMOBIDEX_RELEASE_KEY_PASSWORD= -PMOBIDEX_SIGN=1"
+  ./gradlew assembleRelease -PMOBIDEX_RELEASE_KEY_ALIAS=${MOBIDEX_RELEASE_KEY_ALIAS+mobidex} -PMOBIDEX_RELEASE_STORE_FILE=${MOBIDEX_RELEASE_STORE_FILE+../../mobidex.keystore} -PMOBIDEX_RELEASE_STORE_PASSWORD=${MOBIDEX_RELEASE_STORE_PASSWORD} -PMOBIDEX_RELEASE_KEY_PASSWORD=${MOBIDEX_RELEASE_STORE_PASSWORD} -PMOBIDEX_SIGN=1;
+fi;
+
+popd android
