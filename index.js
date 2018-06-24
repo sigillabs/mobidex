934c7ea1dd98385d9946a27d69e9255e9f80323e
Abraham Elmahrek <abraham@elmahrek.com>
2018-06-23 18:36:09 -0700

    Remove warnings

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import 'node-libs-react-native/globals';
 import { EventEmitter } from 'events';
-import React, { Component } from 'react';
 import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 
-// console.disableYellowBox = true;
-YellowBox.ignoreWarnings([
-  'Class RCTCxxModule',
-  // 'Warning: Failed prop type',
-  // 'Warning: isMounted',
-  'Warning:'
-]);
+YellowBox.ignoreWarnings(['Class RCTCxxModule', 'Warning:', 'Method']);
 EventEmitter.defaultMaxListeners = 1000;
 
 AppRegistry.registerComponent('mobidex', () => App);
