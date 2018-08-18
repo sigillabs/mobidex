b42191c3f3ed925897894ff89ce521c9b7392c20
Abraham Elmahrek <abraham@elmahrek.com>
2018-08-18 15:23:24 -0700

    Remove some files

--- App/components/Divider.js
+++ App/components/Divider.js
@@ -1,11 +1,16 @@
-import React, { Component } from "react";
-import { Divider as RNEDivider } from "react-native-elements";
-import { colors } from "../../styles";
+import React, { Component } from 'react';
+import { Divider as RNEDivider } from 'react-native-elements';
+import { colors } from '../../styles';
 
 export default class Divider extends Component {
   render() {
     return (
-      <RNEDivider style={[{ backgroundColor: colors.grey3, marginTop: 10, marginBottom: 10 }, this.props.style]} />
+      <RNEDivider
+        style={[
+          { backgroundColor: colors.grey3, marginTop: 10, marginBottom: 10 },
+          this.props.style
+        ]}
+      />
     );
   }
 }
