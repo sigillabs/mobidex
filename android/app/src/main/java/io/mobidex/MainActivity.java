dc86c691abb3cb51a39d98919f41da74ae06ffd4
Abraham Elmahrek <abraham@elmahrek.com>
2018-12-05 00:48:48 -0800

    #153: Android splash screen

--- android/app/src/main/java/io/mobidex/MainActivity.java
+++ android/app/src/main/java/io/mobidex/MainActivity.java
@@ -1,5 +1,25 @@
 package io.mobidex;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+
 import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends NavigationActivity {}
+public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.createSplashLayout());
+    }
+
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(), R.drawable.launch_screen);
+        splash.setBackground(launch_screen_bitmap);
+
+        return splash;
+    }
+}
