c17d30ac36d9e97eb5137aaae489e11c82a9d981
Abraham Elmahrek <abraham@elmahrek.com>
2019-01-20 19:17:32 -0800

    Use alchemy insights nodes again

--- reducers/settings.js
+++ reducers/settings.js
@@ -5,10 +5,10 @@ import { ZERO } from '../constants/0x';
 
 const PROD = {
   network: 1,
-  ethereumNodeEndpoint:
-    'https://mainnet.infura.io/v3/9c07eacbc58e42fa9a5b5b19d8992787',
   // ethereumNodeEndpoint:
-  //   'https://eth-mainnet.alchemyapi.io/jsonrpc/x1YomDJDRqqvIAHJNeEhpGoZj6i18ARB',
+  //   'https://mainnet.infura.io/v3/9c07eacbc58e42fa9a5b5b19d8992787',
+  ethereumNodeEndpoint:
+    'https://eth-mainnet.alchemyapi.io/jsonrpc/x1YomDJDRqqvIAHJNeEhpGoZj6i18ARB',
   relayerEndpoint: 'https://mobidex.io/relayer/v2',
   relayerWSS: 'wss://mobidex.io/relayer/v2/',
   inf0xEndpoint: 'https://mobidex.io:9443/inf0x/2.1/',
@@ -17,10 +17,10 @@ const PROD = {
 
 const STAGE = {
   network: 42,
-  // ethereumNodeEndpoint:
-  //   'https://kovan.infura.io/v3/9c07eacbc58e42fa9a5b5b19d8992787',
   ethereumNodeEndpoint:
-    'https://eth-kovan.alchemyapi.io/jsonrpc/x1YomDJDRqqvIAHJNeEhpGoZj6i18ARB',
+    'https://kovan.infura.io/v3/9c07eacbc58e42fa9a5b5b19d8992787',
+  // ethereumNodeEndpoint:
+  //   'https://eth-kovan.alchemyapi.io/jsonrpc/x1YomDJDRqqvIAHJNeEhpGoZj6i18ARB',
   relayerEndpoint: 'https://mobidex.io:9443/relayer/v2',
   relayerWSS: 'wss://mobidex.io:9443/relayer/v2/',
   inf0xEndpoint: 'https://mobidex.io:9443/inf0x/2.1/',
